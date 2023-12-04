5feaa4875a2275e7531c2aecdb425687f5492bd2
vimal <vimalsundharam@gmail.com>
2023-12-05 02:22:06 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import MyClassComponent from './Components/MyClassComponent';
 import { MyFunctionalComponent } from './Components/MyFunctionalComponent';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    
         
         <p>
           <MyClassComponent/>
           <MyFunctionalComponent/>
         </p>
        
-      </header>
-    </div>
+     
+    
   );
 }
 
